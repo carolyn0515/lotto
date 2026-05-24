9112929a3b9228fabeb248516a0e85e585459a98
carolyn <neige040515@gmail.com>
2026-05-24 09:53:36 +0900

    feat: add purchase_manual & my_tickets

--- lotto/urls.py
+++ lotto/urls.py
@@ -3,4 +3,10 @@ from . import views
 app_name = "lotto"
 urlpatterns=[
     path("", views.home, name="home"),
+    path(
+        "draws/<int:draw_id>/purchase/manual/",
+        views.purchase_manual,
+        name="purchase_manual",
+    ),
+    path("my_tickets/", views.my_tickets, name="my_tickets"),
 ]
